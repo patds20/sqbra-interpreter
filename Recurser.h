ba2a297059f568b1afc857abaef809d863e625f7
Patrick De Smet <117687406+patds20@users.noreply.github.com>
2023-04-12 15:49:47 +0200

    Adding Matrix Commands

--- Recurser.h
+++ Recurser.h
@@ -29,8 +29,9 @@ bool if_state = false; // was the previous if or elif false --> enables executio
 
 int execute(Node* node);
 
-// LOAD CONSTANTS ################################################################
+// DEFINED CONSTANTS ################################################################
 
+//! Predefined Constants
 inline void load_math_const(){
     auto predefined = new vector<long double>(7);
     (*predefined)[0] = 3.14159265358979323846264338327;
@@ -50,88 +51,73 @@ inline void load_math_const(){
     vars["capry"] = (*predefined).data() + 6;
 }
 
-// RESOLVE FUNCTIONS ##############################################################
+// AUXILIARY FUNCTIONS ##############################################################
 
+//! Set the Value of a Variable, List or Matrix MathNode
 inline void setVarListEntry(MathNode* varlist, long double value){
     if(varlist->type == MathNodeType::Variable){
         *(long double*)varlist->variable = value;
-
+    }else if(varlist->type == MathNodeType::Array){
+        auto list = reinterpret_cast<vector<long double>*>(varlist->variable);
+        auto i = (int)(calculateExpression(varlist->left));
+        if(list->size() > i){
+            (*list)[i] = value;
+        }else{
+            cerr << "Error: Index out of bounds for list <" << varlist->variable << "> at index <" << i << ">." << endl;
+            exit(0);
+        }
     }else{
-        (*reinterpret_cast<vector<long double>*>(varlist->variable))[(int)(calculateExpression(varlist->left))] = value;
+        auto mat = reinterpret_cast<vector<vector<long double>*>*>(varlist->variable);
+        auto i1 = (int)(calculateExpression(varlist->left));
+        auto i2 = (int)(calculateExpression(varlist->right));
+        if(i1 < mat->size() && i2 < (*(*mat)[i1]).size()){
+            (*(*mat)[i1])[i2] = value;
+        }else{
+            cerr << "Error: Index out of bounds for matrix <" << varlist->variable << "> at indices <" << i1 << "," << i2 << ">." << endl;
+            exit(0);
+        }
     }
 }
 
+//! Get the Value of a Variable, List or Matrix MathNode
 inline long double getVarListEntry(MathNode* varlist){
     if(varlist->type == MathNodeType::Variable){
         return *(double*)varlist->variable;
+    }else if(varlist->type == MathNodeType::Array){
+        auto list = reinterpret_cast<vector<long double>*>(varlist->variable);
+        auto i = (int)(calculateExpression(varlist->left));
+        if(list->size() > i){
+            return (*list)[i];
+        }else{
+            cerr << "Error: Index out of bounds for list <" << varlist->variable << "> at index <" << i << ">." << endl;
+            exit(0);
+        }
     }else{
-        return (*reinterpret_cast<vector<long double>*>(varlist->variable))[(int)(calculateExpression(varlist->left))];
+        auto mat = reinterpret_cast<vector<vector<long double>*>*>(varlist->variable);
+        auto i1 = (int)(calculateExpression(varlist->left));
+        auto i2 = (int)(calculateExpression(varlist->right));
+        if(i1 < mat->size() && i2 < (*(*mat)[i1]).size()){
+            return (*(*mat)[i1])[i2];
+        }else{
+            cerr << "Error: Index out of bounds for matrix <" << varlist->variable << "> at indices <" << i1 << "," << i2 << ">." << endl;
+            exit(0);
+        }
     }
 }
 
+//! Round Value of Variable to Specific Precision
 inline long double round_to(long double value, long double precision) {
     return round(value / precision) * precision;
 }
 
 // COMMAND FUNCTIONS ##############################################################
 
+//! Command: Define A Single Variable (CVAR)
 inline void command_cvar(long double* varname, MathNode* expression){
     *varname = calculateExpression(expression);
 }
 
-inline void command_clist(vector<long double>* listid, MathNode* expression){
-    listid->resize((int)calculateExpression(expression));
-}
-
-inline void command_set(MathNode* varlist, MathNode* expression){
-    setVarListEntry(varlist,calculateExpression(expression));
-}
-
-inline void command_ceil(MathNode* varlist){
-    setVarListEntry(varlist, ceil(getVarListEntry(varlist)));
-}
-
-inline void command_floor(MathNode* varlist){
-    setVarListEntry(varlist, floor(getVarListEntry(varlist)));
-}
-
-inline void command_abs(MathNode* varlist){
-    setVarListEntry(varlist, abs(getVarListEntry(varlist)));
-}
-
-inline void command_round(MathNode* varlist, MathNode* precision){
-    long double prec = calculateExpression(precision);
-    setVarListEntry(varlist, round_to(getVarListEntry(varlist),prec));
-}
-
-inline void command_print(const string& stri){
-    string str = extract_string(stri);
-    cout << str << endl;
-}
-
-inline void command_printb(const string& stri){
-    string str = extract_string(stri);
-    cout << str;
-}
-
-inline void command_printv(MathNode* expression) {
-    cout << calculateExpression(expression); // print output of processed expression
-}
-
-inline void command_inc(MathNode* varlist) {
-    setVarListEntry(varlist, getVarListEntry(varlist) + 1);
-}
-
-inline void command_dec(MathNode* varlist) {
-    setVarListEntry(varlist, getVarListEntry(varlist) - 1);
-}
-
-inline void command_input(MathNode* varlist) {
-    long double inputval;
-    cin >> inputval;
-    setVarListEntry(varlist, inputval);
-}
-
+//! Command: Define Multiple Variables (MVAR)
 inline void command_mvar(vector<long double*>* variables, MathNode* val){
     long double tmp_val = calculateExpression(val);
     for(long double* svars : *variables){
@@ -139,6 +125,90 @@ inline void command_mvar(vector<long double*>* variables, MathNode* val){
     }
 }
 
+//! Command: Define A List (CLIST)
+inline void command_clist(vector<long double>* listid, MathNode* expression){
+    listid->resize((int)calculateExpression(expression));
+}
+
+//! Command: Define A Matrix (CMAT)
+inline void command_cmat(vector<vector<long double>* >* matid, MathNode* dim1, MathNode* dim2){
+    for(int i = 0; i < (int)calculateExpression(dim1); i++){
+        auto vct = new vector<long double>((int)calculateExpression(dim2));
+        matid->push_back(vct);
+    }
+}
+
+//! Command: Assign A Value to Variable or Matrix / List Entry (SET)
+inline void command_set(MathNode* varlist, MathNode* expression){
+    setVarListEntry(varlist,calculateExpression(expression));
+}
+
+//! Command: Round Value of Variable Up (CEIL)
+inline void command_ceil(MathNode* varlist){
+    setVarListEntry(varlist, ceil(getVarListEntry(varlist)));
+}
+
+//! Command: Round Value of Variable Off (FLOOR)
+inline void command_floor(MathNode* varlist){
+    setVarListEntry(varlist, floor(getVarListEntry(varlist)));
+}
+
+//! Command: Get Absolute Value of Variable (ABS)
+inline void command_abs(MathNode* varlist){
+    setVarListEntry(varlist, abs(getVarListEntry(varlist)));
+}
+
+//! Command: Round Value to Specicic Accuracy (ROUND)
+inline void command_round(MathNode* varlist, MathNode* precision){
+    long double prec = calculateExpression(precision);
+    setVarListEntry(varlist, round_to(getVarListEntry(varlist),prec));
+}
+
+//! Command: Print String and Break Line (PRINT)
+inline void command_print(const string& stri){
+    string str = extract_string(stri);
+    cout << str << endl;
+}
+
+//! Command: Print String Without Breaking The Line (PRINTB)
+inline void command_printb(const string& stri){
+    string str = extract_string(stri);
+    cout << str;
+}
+
+//! Command: Print Value of Variable (PRINTV)
+inline void command_printv(MathNode* expression) {
+    cout << calculateExpression(expression); // print output of processed expression
+}
+
+//! Command: Print Matrix to Screen (PRINTM)
+inline void command_printm(vector<vector<long double>*>* matrix) {
+    for(auto row : *matrix){
+        for(auto val : *row){
+            cout << val << " ";
+        }
+        cout << endl;
+    }
+}
+
+//! Command: Increment Value of Variable (INC)
+inline void command_inc(MathNode* varlist) {
+    setVarListEntry(varlist, getVarListEntry(varlist) + 1);
+}
+
+//! Command: Decrement Value of Variable (DEC)
+inline void command_dec(MathNode* varlist) {
+    setVarListEntry(varlist, getVarListEntry(varlist) - 1);
+}
+
+//! Command: Get Input from CLI and Save to Variable (INPUT)
+inline void command_input(MathNode* varlist) {
+    long double inputval;
+    cin >> inputval;
+    setVarListEntry(varlist, inputval);
+}
+
+//! Command: Calculate Trigonometric Function and Save to Target (TRIG)
 inline void command_trig(MathNode* varlist, MathNode* source, TokenType func){
     long double inpVal = calculateExpression(source); // get x
     long double outVal;
@@ -176,10 +246,12 @@ inline void command_trig(MathNode* varlist, MathNode* source, TokenType func){
     setVarListEntry(varlist, outVal);
 }
 
+//! Command: Save the Length of a List (GETL)
 inline void command_getl(MathNode* varlist, vector<long double>* listid){
     setVarListEntry(varlist, (long double)(*reinterpret_cast<vector<long double>*>(listid)).size());
 }
 
+//! Command: Calculate a Specific Root of the Variable and Save it (XROOT)
 inline void command_xroot(MathNode* varlist, MathNode* source, MathNode* root){
     long double inpVal = calculateExpression(source);
     long double roottype = calculateExpression(root);
@@ -187,6 +259,7 @@ inline void command_xroot(MathNode* varlist, MathNode* source, MathNode* root){
     setVarListEntry(varlist, outVal);
 }
 
+//! Command: Calculate a Specific Logarithm of the Variable and Save it (LOG)
 inline void command_log(MathNode* varlist, MathNode* source, MathNode* exp){ //HERE
     long double inpVal = calculateExpression(source);
     long double expVal = calculateExpression(exp);
@@ -194,6 +267,7 @@ inline void command_log(MathNode* varlist, MathNode* source, MathNode* exp){ //H
     setVarListEntry(varlist, outVal);
 }
 
+//! Command: Declare a Void Function Without Parameters (FUNCT)
 inline void command_declarefunct(const string& identifier, Node* exec_block){
     if(funcs.find(identifier) == funcs.end()){
         funcs[identifier] = exec_block;
@@ -203,36 +277,48 @@ inline void command_declarefunct(const string& identifier, Node* exec_block){
     }
 }
 
+//! Command: Pop The Last Value Of a List And Save It To a Variable(POP)
 inline void command_poplist(MathNode* varlist, vector<long double>* listid){
     setVarListEntry(varlist, listid->back());
     listid->pop_back();
 }
 
+//! Command: Push A Specific Value to A List (PUSH)
 inline void command_pushlist(MathNode* expr, vector<long double>* listid){
     long double val = calculateExpression(expr);
     listid->push_back(val);
 }
 
+//! Command: Change The Size of A List (CHSL)
 inline void command_chsl(MathNode* expr, vector<long double>* listid){
     int new_l = (int)calculateExpression(expr);
     listid->resize(new_l);
 }
 
+//! Command: Define a List With Predefined Values (LDEF)
 inline void command_ldef(vector<long double>* listid, string* stri){
     string content = removeSpaces(extract_list_string(*stri));
     parse_numbers(content,listid);
 }
 
+//! Command: Define a Matrix With Predefined Values (MDEF)
+inline void command_mdef(vector<vector<long double>* >* matid, string* stri){
+    parse_matrix(*stri,matid);
+}
+
+//! Command: Read a CSV File and Save it to a List (READF)
 inline void command_readf(vector<long double>* listid, string* filename){
     string filepath = extract_string(*filename);
     read_csv(filepath,listid);
 }
 
+//! Command: Write a List to a CSV File (WRITEF)
 inline void command_writef(vector<long double>* listid, string* filename){
     string filepath = extract_string(*filename);
     write_csv(filepath,listid);
 }
 
+//! Command: Define a Random Value in the Closed Interval From Max to Min and Save it to a Variable (RANDOM)
 inline void command_random(MathNode* varlist, MathNode* max, MathNode* min){
     std::random_device rd;
     std::seed_seq seed{ rd(), static_cast<uint32_t>(std::chrono::high_resolution_clock::now().time_since_epoch().count()) };
@@ -243,6 +329,7 @@ inline void command_random(MathNode* varlist, MathNode* max, MathNode* min){
 
 // MAIN FUNCTIONS FOR THE EXECUTION OF COMMANDS
 
+//! Determine the Specific Function to Call for The Command
 inline void executeCommand(Node* statement) {
     switch (statement->type) {
         case CVAR:
@@ -260,6 +347,9 @@ inline void executeCommand(Node* statement) {
         case PRINTV:
             command_printv(statement->children[0]->expression);
             break;
+        case PRINTM:
+            command_printm((vector<vector<long double>* >*)statement->children[0]->value);
+            break;
         case DEC:
             command_dec(statement->children[0]->expression);
             break;
@@ -268,6 +358,9 @@ inline void executeCommand(Node* statement) {
             break;
         case CLIST:
             command_clist((vector<long double>*)statement->children[0]->value, statement->children[1]->expression);
+            break;
+        case CMAT:
+            command_cmat((vector<vector<long double>* >*)statement->children[0]->value, statement->children[1]->expression, statement->children[2]->expression);
             break;
         case INPUT:
             command_printb(*(string*)statement->children[1]->value);
@@ -348,12 +441,16 @@ inline void executeCommand(Node* statement) {
         case LDEF:
             command_ldef((vector<long double>*)statement->children[0]->value, (string*)statement->children[1]->value);
             break;
+        case MDEF:
+            command_mdef((vector<vector<long double>* >*)statement->children[0]->value, (string*)statement->children[1]->value);
+            break;
         case RANDOM:
             command_random(statement->children[0]->expression,statement->children[1]->expression,statement->children[2]->expression);
             break;
     }
 }
 
+//! Execute the Tree Recursively and Implement Control Structures
 int execute(Node* node){
     int loopc;
     switch (node->type) {
