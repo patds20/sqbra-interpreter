ff5d9e70077343c91c2328339395e817c68ecd96
Patrick De Smet <117687406+patds20@users.noreply.github.com>
2023-04-12 15:47:25 +0200

    New command for defining a matrix

--- FilesIO.h
+++ FilesIO.h
@@ -12,6 +12,7 @@
 #include <iostream>
 #include <fstream>
 
+//! Extract [" and "] from strings
 inline string extract_string(const string& input) {
     std::size_t start_pos = input.find("[\"");
     std::size_t end_pos = input.find("\"]");
@@ -22,6 +23,7 @@ inline string extract_string(const string& input) {
     return input.substr(start_pos + 2, end_pos - start_pos - 2);
 }
 
+//! Extract outer [ and ] from strings containing lists or matrices
 inline string extract_list_string(const string& input) {
     std::size_t start_pos = input.find('[');
     std::size_t end_pos = input.find(']');
@@ -32,6 +34,7 @@ inline string extract_list_string(const string& input) {
     return input.substr(start_pos + 1, end_pos - start_pos - 1);
 }
 
+//! Read and input csv files
 inline void read_csv(const std::string& filename, std::vector<long double>* output) {
     std::ifstream file(filename);
     if (!file.is_open()) {
@@ -54,13 +57,13 @@ inline void read_csv(const std::string& filename, std::vector<long double>* outp
     }
 }
 
+//! Write lists to csv files
 inline void write_csv(const std::string& filename, std::vector<long double>* numbers) {
     std::ofstream file(filename);
     if (!file.is_open()) {
         std::cerr << "Error: Unable to open file: " << filename << std::endl;
         exit(0);
     }
-
     for (const auto& number : *numbers) {
         file << number << ",";
     }
@@ -69,6 +72,7 @@ inline void write_csv(const std::string& filename, std::vector<long double>* num
     file << std::endl;
 }
 
+//! Parse a string [0.32, 0.435, 3.141] to a list
 inline void parse_numbers(const std::string& input, std::vector<long double>* output) {
     std::istringstream ss(input);
     std::string token;
@@ -79,6 +83,40 @@ inline void parse_numbers(const std::string& input, std::vector<long double>* ou
         } catch (const std::exception& e) {
             // Handle conversion error
         }
+    }
+}
+
+//! Parse a string [[1,2,3],[4,5,6],[7,8,9]] to a matrix
+inline void parse_matrix(string& matrix_string, vector<vector<long double>* >* matrix)
+{
+    // Remove the outermost brackets from the matrix string
+    std::string stripped_string = matrix_string.substr(1, matrix_string.length() - 2);
+
+    // Split the matrix string into rows
+    std::stringstream row_stream(stripped_string);
+    std::string row_string;
+    while (std::getline(row_stream, row_string, ']'))
+    {
+        // Remove the leading "," character from each row string
+        if(row_string[0] == ','){
+            row_string = row_string.substr(1);
+        }
+
+        // Remove the leading "[" character from each row string
+        row_string = row_string.substr(1);
+
+        // Parse the row string into a vector of long doubles
+        auto row = new std::vector<long double>();
+        std::stringstream value_stream(row_string);
+        std::string value_string;
+        while (std::getline(value_stream, value_string, ','))
+        {
+            long double value = std::stold(value_string);
+            row->push_back(value);
+        }
+
+        // Add the row to the matrix
+        matrix->push_back(row);
     }
 }
 
