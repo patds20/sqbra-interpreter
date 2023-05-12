9f533b44ba36e66fbc1d2b3271845f74c94b0a10
Patrick De Smet <117687406+patds20@users.noreply.github.com>
2023-05-13 00:31:54 +0200

    Update Tokens.h

--- Tokens.h
+++ Tokens.h
@@ -4,17 +4,63 @@
 
 /*
  *  TOKENS.H
- *  This file defines the Node structure for tree and all the possible commands.
+ *  This file declares the required funtions and variables for the Parser and all possible tokens.
  *  Copyright (c) 2023, Patrick De Smet
  */
 
 #include <string>
 #include <vector>
-#include "MathCore.cpp"
+#include <iostream>
+#include <sstream>
+#include <string>
+#include <stack>
+#include <cmath>
+#include <unordered_map>
+#include <vector>
+#include <iterator>
 
-using namespace std;
+/// MathCore Declarations #############
 
-//! Enum for Token Types
+extern std::unordered_map<std::string, long double*> vars; // global map for all vars (only required during parsing)
+extern std::unordered_map<std::string, std::vector<long double>*> ars; // global map for all arrays (only required during parsing)
+extern std::unordered_map<std::string, std::vector<std::vector<long double>* >*> mars; // global map for all matrices (only required during parsing)
+
+enum class MathNodeType { Constant, Variable, Array, Matrix, Operator };
+enum class MathOperatorType { PLUS, MINUS, MULT, DIV, MOD, EXP, AND, OR, EQ, NEQ, LARGER, LARGEREQ, LESS, LESSEQ, NOTSET };
+
+MathOperatorType getOperatorType(const char c);
+
+struct MathNode {
+    MathNodeType type;
+    long double constant;
+    MathOperatorType opt;
+    void* variable;
+    MathNode* left;
+    MathNode* right;
+    MathNode(MathNodeType t, void* v) : type(t), variable(v), constant(0), left(nullptr), right(nullptr), opt(MathOperatorType::NOTSET) {}
+    MathNode(MathNodeType t, long double v) : type(t), constant(v), left(nullptr), right(nullptr), opt(MathOperatorType::NOTSET) {}
+    MathNode(MathNodeType t, MathOperatorType s) : type(t), variable(nullptr), constant(0), left(nullptr), right(nullptr), opt(s) {}
+};
+
+struct MathTypeReturn {
+    MathNodeType type;
+    std::string var;
+    std::string ind1;
+    std::string ind2;
+};
+
+long double calculateExpression(MathNode* root);
+
+inline std::string getOperator(MathOperatorType type);
+
+MathNode* mathparse(const std::string& expr);
+
+MathNode* varparse(const std::string& expr);
+
+std::string getOperator(MathOperatorType type);
+
+// TOKENTYPE (all possible tokens)
+
 enum TokenType {
     ROOT,
     CVAR,
@@ -30,6 +76,7 @@ enum TokenType {
     ROUND,
     LOOP,
     SLOOP,
+    AUTOLOOP,
     WHILE,
     INC,
     DEC,
@@ -49,6 +96,7 @@ enum TokenType {
     VARLIST,
     EXEC,
     EXIT,
+    SLEEP,
     LEAVE,
     LOG,
     EXP,
@@ -75,7 +123,7 @@ enum TokenType {
     READF,
     WRITEF,
     RANDOM
-}; // the type of data (type of command, expression, string, ...)
+};
 
 class Node {
 public:
@@ -83,6 +131,16 @@ public:
     std::vector<Node*> children;
     void *value;
     MathNode* expression;
-}; // nodes contain commands, expressions, variables, values, statements, ...
+};
+
+/// FUNCTIONS
+
+Node* parseTree(const std::vector<std::vector<std::string> >& tokens, int startLine);
+
+void printMathTree(MathNode* root, int level);
+
+void printTree(Node* root, int level);
+
+void printTreeRoot(Node* root);
 
 #endif //SQBRA_TOKENS_H
